21d6b28a533e68b88d40c036e151db3bc58f5f3a
Brady <bradyrcotton@gmail.com>
2021-05-05 09:52:33 -0500

    created app.js and index.js

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './Components/app';
+import 'bootstrap/dist/css/bootstrap.css';
+
+const jsxElement = <h1>Youtube Clone App</h1>;
+console.log(jsxElement);
+
+ReactDOM.render(<App/>, document.getElementById('root'));
